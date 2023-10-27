780dd4338926f9e1f0b470729f5e36e99511db32
kiran madiwalar <kiranm81040@gmail.com>
2023-10-27 11:35:07 +0530

    revision started

---
+++ script.js
@@ -0,0 +1,10 @@
+// const btn = document.getElementById("btn");
+// const head = document.getElementById("h1");
+// btn.onclick("click", () => {
+//   head.innerText = "i am changed";
+// });
+
+// let arr1 = [11, 44, 45, 101];
+
+// arr1.unshift(45);
+// console.log(arr1);
